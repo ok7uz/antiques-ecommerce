e65057dc7cf05dfa3477cf409d9a1e76d95e91cd
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-03-22 17:50:19 +0500

    .env

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,9 +6,9 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # Example engine (change if needed)
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'decor$2001',
         'HOST': 'localhost',
         'PORT': '5432',
     }
