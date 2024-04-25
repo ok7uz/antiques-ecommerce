79397aded97f85806d8b01446ad154b80cbfa571
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-04-25 16:07:18 +0500

    changed migrations files

--- apps/main_page/migrations/0002_banner_button_url_banner_has_button.py
+++ apps/main_page/migrations/0002_banner_button_url_banner_has_button.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main_page', '0004_alter_video_type'),
+        ('main_page', '0001_initial'),
     ]
 
     operations = [
