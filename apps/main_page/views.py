ab7be09a9d1ef16331646a72c79bdee801c65317
ok7uz <komronbekobloev@gmail.com>
2024-06-19 16:16:52 +0500

    callback via email

--- apps/main_page/views.py
+++ apps/main_page/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from apps.main_page.models import News, Banner, Video, Expert
 from apps.main_page.serializers import NewsListSerializer, NewsSerializer, BannerSerializer, VideoSerializer, \
     ExpertSerializer
-from config.settings.base import EMAIL_HOST_USER
+from config.settings.base import EMAIL_HOST_USER, RECIPIENT_EMAIL
 
 
 class NewsView(APIView):
@@ -34,13 +34,6 @@ class NewsListView(APIView):
         tags=['Main Page'],
     )
     def get(self, request):
-        send_mail(
-            'Test Subject',
-            'Test message body',
-            EMAIL_HOST_USER,
-            ['komronbekobloev@gmail.com'],
-            fail_silently=False,
-        )
         queryset = News.objects.all()
         serializer = NewsListSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,7 @@
+from smtplib import SMTPDataError
+from django.core.mail import EmailMultiAlternatives
+
+from django.core.mail import send_mail
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.permissions import AllowAny
@@ -5,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.order.serializers import OrderSerializer, CallbackSerializer
+from config.settings.base import EMAIL_HOST_USER, RECIPIENT_EMAIL
 
 
 class OrderView(APIView):
@@ -37,5 +42,19 @@ class CallbackView(APIView):
 
         if serializer.is_valid(raise_exception=True):
             serializer.save()
+            applicant_name = serializer.data.get('applicant_name')
+            applicant_email = serializer.data.get('applicant_email')
+            message = 'Имя: ' + applicant_name + '\n' + 'Email: ' + applicant_email
+            try:
+                applicant_name = serializer.data.get('applicant_name')
+                applicant_email = serializer.data.get('applicant_email')
+                text_content = 'Имя: ' + applicant_name + '\n' + 'Email: ' + applicant_email
+                html_content = '<b>Имя:</b>  ' + applicant_name + '<br>' + '<b>Email:</b>  ' + applicant_email
+                subject, from_email, to = 'ОБРАТНАЯ СВЯЗЬ', EMAIL_HOST_USER, RECIPIENT_EMAIL
+                msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+                msg.attach_alternative(html_content, "text/html")
+                msg.send()
+            except SMTPDataError as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
             return Response({'success': True, 'message': 'Callback data saved!'}, status=status.HTTP_200_OK)
         return Response({'success': False, 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
